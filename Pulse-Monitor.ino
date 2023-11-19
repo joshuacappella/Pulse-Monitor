23c8c17fb03a3bbc8f86692d32d605ce38f972ba
Joshua Cappella <sirissuperspecialsatellite@Siris-Super-Special-Satellite.local>
2023-11-18 21:51:40 -0500

    Audio functions added

--- Pulse-Monitor.ino
+++ Pulse-Monitor.ino
@@ -5,7 +5,7 @@
  * Wrist-mounted monitor for tracking heart rate
  */
 
-#include <Wire.h>
+include <Wire.h>
 //might be worth considering rewriting some of the MAX30100 library functionality
 //this would increase lines of code written, but would allow us to learn
 //more, and keep the project more our own
@@ -14,6 +14,22 @@
 #include "MAX30100_PulseOximeter.h"
 
 #define REPORTING_PERIOD_MS     1000
+#define NOTE_A5S 932.3
+#define NOTE_A5  880.0
+#define NOTE_G5  784.0
+#define NOTE_F5  698.5
+#define NOTE_E5  659.3
+#define NOTE_D5  587.3
+#define NOTE_C5  523.3
+#define NOTE_B4  493.9
+#define NOTE_A4  440.0
+#define NOTE_E4  329.6
+#define NOTE_D4S 311.1
+#define NOTE_B3  246.9
+#define NOTE_G3S 207.7 
+#define NOTE_REST 0
+
+int speakerPin = 8; // Speaker connected to D8
 
 PulseOximeter pox;
 
@@ -26,6 +42,7 @@ void onBeatDetected()
 
 void setup()
 {
+    pinMode(speakerPin, OUTPUT);	//Setup up speaker pin for output 
     Serial.begin(115200);
 
     if (!pox.begin()) {
@@ -37,6 +54,7 @@ void setup()
 
     pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
 }
+
 
 void loop()
 {
@@ -51,4 +69,36 @@ void loop()
 
         tsLastReport = millis();
     }
+    playCrabRave();       //Recommended for low heart rate, call when lower limit crossed
+    playShootingStars();  //Recommended for high heart rate, call when upper limit crossed
 }
+
+void playCrabRave() {
+    // Define note frequencies and durations
+    float crabRave[] = {NOTE_D5, NOTE_A5S, NOTE_G5, NOTE_G5, NOTE_D5, NOTE_D5, NOTE_A5, NOTE_F5, NOTE_F5, NOTE_D5, NOTE_D5, NOTE_A5, NOTE_F5, NOTE_F5, NOTE_C5, NOTE_C5, NOTE_E5, NOTE_E5, NOTE_F5};
+int noteDurations[] = {187, 187, 187, 103, 187, 103, 187, 187, 103, 187, 103, 187, 187, 103, 187, 187, 187, 103, 187}; // in milliseconds, adjusted for 30ms playNote delay
+    // Play song
+    for (int i = 0; i < sizeof(crabRave) / sizeof(crabRave[0]); i++) {
+      playNote(crabRave[i], noteDurations[i]);
+      delay(10); // Add a small delay between notes
+    }
+}
+
+void playShootingStars() {
+    // Define note frequencies and durations
+    float shootingStars[] = {NOTE_D4S, NOTE_REST, NOTE_D4S, NOTE_E4, NOTE_REST, NOTE_B3, NOTE_G3S};
+    int noteDurations[] = {520, 120, 170, 295, 70, 170, 150}; // in milliseconds, adjusted for 30ms playNote delay
+
+    // Play song
+    for (int i = 0; i < sizeof(shootingStars) / sizeof(shootingStars[0]); i++) {
+      playNote(shootingStars[i], noteDurations[i]);
+      delay(30); // Add a small delay between notes
+    }
+}
+
+void playNote(float noteFrequency, int duration) {
+    tone(speakerPin, noteFrequency, duration);
+    delay(duration + 30); // Add a small delay between notes (30)
+    noTone(speakerPin);
+}
+
